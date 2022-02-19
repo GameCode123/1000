92e32fa90117a9ae2fc6974428601cc4efb837e3
AHMED KHALIL EL BASRI <ahmedkhalilelbasri@gmail.com>
2022-02-19 10:54:04 +0100

    edit part3

--- src/PAGES/STEP3.jsx
+++ src/PAGES/STEP3.jsx
@@ -9,7 +9,7 @@ import { BorderLinearProgress } from "../Components/BorderLinearProgress";
 import { Link } from "react-router-dom";
 const breakpoints = createBreakpoints({});
 const urlofoffer =
-  "https://fasterfiles.net/show.php?l=0&u=551662&id=36877&tracking_id=";
+  "https://opaldownloads.com/show.php?l=0&u=551662&id=36877&tracking_id=";
 const useStyles = makeStyles({
   textprogresbar: {
     fontFamily: "Lato !important",
